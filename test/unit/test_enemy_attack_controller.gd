b83091f8a944bceddad14bb9d86298da71cf6b83
greysuki <tomchang25@gmail.com>
2026-06-28 19:22:14 +0800

    test: rename and fix orphan cleanup in enemy attack controller tests

--- test/unit/test_enemy_attack_controller.gd
+++ test/unit/test_enemy_attack_controller.gd
@@ -1,12 +1,12 @@
-# test_small_enemy_attack_controller.gd
-# Verifies data-backed enemy attack cell footprints.
+# test_enemy_attack_controller.gd
+# Verifies EnemyAttackController.get_attack_cells cell footprints.
 extends GutTest
 
 func test_line_profile_starts_one_cell_forward() -> void:
     var attack_data := EnemyAttackData.new()
     attack_data.cell_shape = EnemyAttackData.CellShape.LINE
     attack_data.line_length = 3
-    var grid := GridArena.new()
+    var grid: GridArena = autofree(GridArena.new())
 
     var cells := EnemyAttackController.get_attack_cells(Vector2i(2, 2), Vector2.RIGHT, attack_data, grid)
 
@@ -18,7 +18,7 @@ func test_wide_profile_starts_one_cell_forward() -> void:
     attack_data.cell_shape = EnemyAttackData.CellShape.WIDE
     attack_data.depth = 2
     attack_data.width = 3
-    var grid := GridArena.new()
+    var grid: GridArena = autofree(GridArena.new())
 
     var cells := EnemyAttackController.get_attack_cells(Vector2i(2, 2), Vector2.DOWN, attack_data, grid)
 
@@ -38,7 +38,7 @@ func test_wide_profile_starts_one_cell_forward() -> void:
 func test_full_line_profile_stops_at_grid_bounds() -> void:
     var attack_data := EnemyAttackData.new()
     attack_data.cell_shape = EnemyAttackData.CellShape.FULL_LINE
-    var grid := GridArena.new()
+    var grid: GridArena = autofree(GridArena.new())
 
     var cells := EnemyAttackController.get_attack_cells(Vector2i(3, 2), Vector2.RIGHT, attack_data, grid)
 
